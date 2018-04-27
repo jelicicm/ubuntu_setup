2b6335e5c304dc76a3eaccddde4d820a5e0734a6
Mirko Jelicic <mirkojelicic991@gmail.com>
2018-04-27 19:00:07 +0200

    added screenfetch

--- install_sw.sh
+++ install_sw.sh
@@ -12,6 +12,7 @@ apt install libgconf2-4 libnss3-1d libxss1 -y
 apt install compizconfig-settings-manager -y
 apt install compiz-plugins -y
 apt install unity-tweak-tool -y
+apt install screenfetch -y
 apt install doublecmd-gtk -y
 apt install python3.6 -y
 apt install sublime-text-installer -y
