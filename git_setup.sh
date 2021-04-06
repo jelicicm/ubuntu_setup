6978091a5361fd51b68f70aada99bf41e0501a87
Mirko Jelicic <mirkojelicic991@gmail.com>
2021-04-06 08:13:05 +0200

    Updated version

--- git_setup.sh
+++ git_setup.sh
@@ -6,5 +6,3 @@ sudo apt install git -y
 git config --global user.name "Mirko Jelicic"
 git config --global user.email "mirkojelicic991@gmail.com"
 
-#Perform Git Pull of useful scripts
-cd ~/Documents/ && git clone https://github.com/jelicicm/ubuntu_setup.git && cd ~/Documents/ubuntu_setup
