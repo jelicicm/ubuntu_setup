5435ea291d3640d99a314917d6fb9e5d07992652
Mirko Jelicic <mirko.jelicic@anari.ai>
2021-04-14 18:10:28 +0200

    Added ubuntu mainline kernel submodule

--- sw.sh
+++ sw.sh
@@ -4,8 +4,6 @@
 sudo add-apt-repository universe
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo add-apt-repository ppa:videolan/stable-daily -y
-sudo add-apt-repository ppa:otto-kesselgulasch/gimp -y
 
 sudo apt update
 sudo apt upgrade -y
@@ -35,18 +33,19 @@ sudo apt install terminator -y
 sudo apt install doublecmd-gtk -y
 sudo apt install gparted -y
 sudo apt install clipit -y
-sudo apt install code -y
+sudo snap install code
 
 # Apps
 sudo apt install chromium-browser -y
 sudo apt install qbittorrent -y
 sudo apt install vlc -y
 pip3 install https://github.com/Diaoul/subliminal/archive/develop.zip
-sudo apt install gimp -y
+sudo snap install gimp
 sudo apt install audacity -y
 
 # Communications
-sudo apt install slack -y
+sudo snap install slack --classic
+sudo snap install zoom-client
 #download Skype for Linux
 wget https://go.skype.com/skypeforlinux-64.deb
 sudo apt install ./skypeforlinux-64.deb
